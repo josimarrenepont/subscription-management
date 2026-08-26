62c18c9be2e7b02a343ee266b77bc66e28756c03
josimarrenepont <jnrenepont@hotmail.com>
2026-08-26 18:15:49 -0300

    feat: add SubscriptionStatus

--- src/main/java/com/subscription_management/subscription_service/core/domain/Subscription.java
+++ src/main/java/com/subscription_management/subscription_service/core/domain/Subscription.java
@@ -11,12 +11,17 @@ import java.time.LocalDateTime;
 public class Subscription {
 
     private final Long id;
+    private final Customer customer;
+    private final Plan plan;
+    private SubscriptionStatus status;
     private LocalDateTime endTime;
     private LocalDateTime nextBillingDate;
     private BigDecimal lastPaymentAmount;
     private String paymentMethod;
 
-    public Subscription(Long id) {
+    public Subscription(Long id, Customer customer, Plan plan) {
         this.id = id;
+        this.customer = customer;
+        this.plan = plan;
     }
 }
---
+++ src/main/java/com/subscription_management/subscription_service/core/domain/SubscriptionStatus.java
@@ -0,0 +1,8 @@
+package com.subscription_management.subscription_service.core.domain;
+
+public enum SubscriptionStatus {
+    ACTIVE,
+    CANCELLED,
+    EXPIRED,
+    PENDING_PAYMENT
+}
