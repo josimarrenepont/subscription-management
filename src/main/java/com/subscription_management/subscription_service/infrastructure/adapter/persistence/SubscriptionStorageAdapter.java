e7f983d367e9697312818a92f7f5e75d32812553
josimarrenepont <jnrenepont@hotmail.com>
2026-09-03 23:38:28 -0300

    feat(infrastructure): add SubscriptionStorageAdapter methods

--- src/main/java/com/subscription_management/subscription_service/infrastructure/adapter/persistence/SubscriptionStorageAdapter.java
+++ src/main/java/com/subscription_management/subscription_service/infrastructure/adapter/persistence/SubscriptionStorageAdapter.java
@@ -2,6 +2,13 @@ package com.subscription_management.subscription_service.infrastructure.adapter.
 
 import com.subscription_management.subscription_service.core.domain.Subscription;
 import com.subscription_management.subscription_service.core.port.SubscriptionStoragePort;
+import com.subscription_management.subscription_service.infrastructure.adapter.persistence.entity.CustomerEntity;
+import com.subscription_management.subscription_service.infrastructure.adapter.persistence.entity.PlanEntity;
+import com.subscription_management.subscription_service.infrastructure.adapter.persistence.mapper.SubscriptionPersistenceMapper;
+import com.subscription_management.subscription_service.infrastructure.adapter.repository.CustomerRepository;
+import com.subscription_management.subscription_service.infrastructure.adapter.repository.PlanRepository;
+import com.subscription_management.subscription_service.infrastructure.adapter.repository.SubscriptionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -9,18 +16,38 @@ import java.util.Optional;
 @Component
 public class SubscriptionStorageAdapter implements SubscriptionStoragePort {
 
+    private final SubscriptionRepository subscriptionRepository;
+    private final CustomerRepository customerRepository;
+    private final PlanRepository planRepository;
+
+    public SubscriptionStorageAdapter(SubscriptionRepository subscriptionRepository, CustomerRepository customerRepository, PlanRepository planRepository) {
+        this.subscriptionRepository = subscriptionRepository;
+        this.customerRepository = customerRepository;
+        this.planRepository = planRepository;
+    }
+
     @Override
     public Subscription save(Subscription subscription) {
-        return null;
+        CustomerEntity customerEntity = customerRepository.findById(subscription.getCustomer().getId())
+                .orElseThrow(() -> new IllegalArgumentException("Customer not found ID"));
+
+        PlanEntity planEntity = planRepository.findById(subscription.getPlan().getId())
+                .orElseThrow(() -> new IllegalArgumentException("Plan not found ID"));
+
+        var entityToSave = SubscriptionPersistenceMapper.toEntity(subscription, customerEntity, planEntity);
+        var savedEntity = subscriptionRepository.save(entityToSave);
+
+        return SubscriptionPersistenceMapper.toDomain(savedEntity);
     }
 
     @Override
     public Optional<Subscription> findById(Long id) {
-        return Optional.empty();
+        return subscriptionRepository.findById(id)
+                .map(SubscriptionPersistenceMapper::toDomain);
     }
 
     @Override
     public void delete(Long id) {
-
+        subscriptionRepository.deleteById(id);
     }
 }
