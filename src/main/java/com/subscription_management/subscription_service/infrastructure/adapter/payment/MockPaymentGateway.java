fb1ae7e4f7a5f64193003be57f522737d9814389
josimarrenepont <jnrenepont@hotmail.com>
2026-09-04 21:40:56 -0300

    feat(infrastructure): add MockPaymentGateway method

--- src/main/java/com/subscription_management/subscription_service/infrastructure/adapter/payment/MockPaymentGateway.java
+++ src/main/java/com/subscription_management/subscription_service/infrastructure/adapter/payment/MockPaymentGateway.java
@@ -1,4 +1,16 @@
 package com.subscription_management.subscription_service.infrastructure.adapter.payment;
 
-public class MockPaymentGateway {
+import com.subscription_management.subscription_service.core.port.PaymentGatewayPort;
+
+import java.math.BigDecimal;
+
+public class MockPaymentGateway implements PaymentGatewayPort {
+
+    @Override
+    public boolean charge(BigDecimal amount, String customerId, String paymentMethod) {
+        System.out.println("Processing payment of R$ " + amount + " for customer " + customerId +
+                " using " + paymentMethod);
+
+        return true;
+    }
 }
